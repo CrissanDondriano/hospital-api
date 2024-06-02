707709fd37a16adbbe099e130bc27390ad6e7ebf
Crissan Dondriano <dondrianocrissan20@gmail.com>
2024-06-02 20:49:07 +0800

    adding routes for method in doctor controller

--- routes/api.php
+++ routes/api.php
@@ -3,20 +3,12 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\DoctorController;
 
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
-
-Route::get('/products', [ProductController::class, 'index']);
-Route::post('/newProducts', [ProductController::class, 'store']);
-Route::get('/getProducts/{id}', [ProductController::class, 'show']);
-Route::put('/update/Products/{id}', [ProductController::class, 'update']);
-Route::delete('/deleteProducts/{id}', [ProductController::class, 'destroy']);
-Route::get('/searchProducts/{title}', [ProductController::class, 'search']);
-
 
 Route::get('/user', [UserController::class, 'index']);
 Route::post('/newUser', [UserController::class, 'store']);
@@ -24,3 +16,10 @@ Route::get('/getUser/{id}', [UserController::class, 'show']);
 Route::put('/update/User/{id}', [UserController::class, 'update']);
 Route::delete('/deleteUser/{id}', [UserController::class, 'destroy']);
 Route::get('/searchUser/{title}', [UserController::class, 'search']);
+
+// DOCTOR CONTROLLER API ENDPOINTS
+Route::get('/doctors', [DoctorController::class, 'index']);
+Route::post('/doctors', [DoctorController::class, 'store']);
+Route::get('/doctors/{id}', [DoctorController::class, 'show']);
+Route::put('/doctors/{id}', [DoctorController::class, 'update']);
+Route::delete('/doctors/{id}', [DoctorController::class, 'destroy']);
